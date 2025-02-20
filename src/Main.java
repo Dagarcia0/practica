1dfd482d5abb833b1dd813da90f54aae85ab8019
Dagarcia0 <dagarciaolaya@ucundinamarca.edu.co>
2025-02-20 13:24:12 -0500

    pratica 8

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(" practica ");
+        System.out.println("practica 8");
     }
 }
